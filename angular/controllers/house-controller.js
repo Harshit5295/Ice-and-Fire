17ee5a758bd531134dd1c4e2b47fbb3460c72680
nilesh_k <nilesh_k@hcl.com>
2017-11-19 11:38:16 +0530

    My commit

---
+++ angular/controllers/house-controller.js
@@ -0,0 +1,25 @@
+app.controller('houseController',['$http','gotservice',function ($http, gotservice) {
+	var main = this;
+	this.houseData = [];
+
+
+	//Function Calling
+	this.allHouse = function() {
+		for(var i = 1; i<50; i++){
+
+		gotservice.getHouseDetail(i)
+		.then(function successCallback(response){
+			main.houseData.push.apply(main.houseData, response.data);
+			//console.log(response.data);
+
+
+		},function errorCallback(response){
+			alert("Some error in GET method");
+		}
+		)
+	}}
+
+
+    this.orderByField = 'name','region';
+	this.reverseSort = false;
+}])
